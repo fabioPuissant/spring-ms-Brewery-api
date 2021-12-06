15d7b76719f28245f0be152e0c48aa4822cbcaca
FabioP <fabio.puissant@hotmail.com>
2021-12-06 23:00:24 +0100

    added Bean validation and Tests for the validations

--- src/main/java/com/fabio/Brewery/web/controller/BeerController.java
+++ src/main/java/com/fabio/Brewery/web/controller/BeerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @Deprecated
@@ -30,7 +31,7 @@ public class BeerController {
      * @return HTTP-response status: 201-CREATED with the location of the newly created Beer object
      */
     @PostMapping // POST - create new beer
-    public ResponseEntity handlePost(@RequestBody  BeerDto beerDto){
+    public ResponseEntity handlePost(@Valid @RequestBody  BeerDto beerDto){
         BeerDto saveDto = beerService.saveNewBeer(beerDto);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", "/api/v1/beer/" + saveDto.getId().toString());
@@ -40,7 +41,7 @@ public class BeerController {
 
     @PutMapping("/{beerId}")
     public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId,
-                                       @RequestBody BeerDto beerDto){
+                                       @Valid @RequestBody BeerDto beerDto){
            beerService.updateBeer(beerId, beerDto);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
